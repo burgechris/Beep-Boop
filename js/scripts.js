a1c7c919906fc56eda61b901bc1c9c7eea630280
Chris Burge <topher.burge@gmail.com>
2019-08-23 08:31:59 -0700

    add code to js file and fill out html file.

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+//User-Interface Logic
+
+$(document).ready(function() {
+  $('#numberChecker').submit(function(event) {
+    event.preventDefault();
+    var userNumber = parseInt($('input#random').val());
+    
+  });
+});
