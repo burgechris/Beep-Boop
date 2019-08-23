c0b44db2153cafd7f095420662e47320ddb94cbd
Chris Burge <topher.burge@gmail.com>
2019-08-23 09:45:57 -0700

    add if/else statements.

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,18 @@ $(document).ready(function() {
   $('#numberChecker').submit(function(event) {
     event.preventDefault();
     var userNumber = $('input#random').val();
+    var range = '';
 
     for(var i = 0; i <= userNumber; i += 1) {
-      if (userNumber[i] === '1') {
-        return 'beep';
-        console.log('beep');
+      if (userNumber.includes('3')){
+        var dave = "I'm sorry, Dave. I'm afraid I can't do that."
+        console.log(dave);
+      } else if (userNumber.includes('2')) {
+        var beep = 'Boop!';
+        console.log(beep);
+      } else if (userNumber.includes('1')) {
+        var beep = 'Beep!';
+        console.log(beep);
       }
     }
   });
