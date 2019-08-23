bed383f24946bba291955c0051e68d13376488dd
Chris Burge <topher.burge@gmail.com>
2019-08-23 15:05:25 -0700

    debug some issues.

--- js/scripts.js
+++ js/scripts.js
@@ -3,25 +3,8 @@
 $(document).ready(function() {
   $('#numberChecker').submit(function(event) {
     event.preventDefault();
-    var userInput = $('input#random').val();
+    var userInput = $('#random').val();
     var display = result(userInput);
-    console.log(display);
-    var output = '';
-
-    function result(userInput) {
-      if (userInput.includes('3')) {
-         return "I'm sorry, Dave. I'm afraid I can't do that."
-      } else if (userInput.includes('2')) {
-         return 'Boop!';
-      } else if (userInput.includes('1')) {
-        return 'Beep!';
-      } else {
-        for (var i = 0; i <= parseInt(userInput); i++) {
-          output += i;
-          return output;
-        }
-      }
-    };
 
     $('#output').text(display);
 
@@ -29,3 +12,19 @@ $(document).ready(function() {
 });
 
 //Business Logic
+
+function result(userInput) {
+  if (userInput.includes('3')) {
+    return "I'm sorry, Dave. I'm afraid I can't do that."
+  } else if (userInput.includes('2')) {
+    return 'Boop!';
+  } else if (userInput.includes('1')) {
+    return 'Beep!';
+  }
+
+    for (var i = 0; i <= parseInt(userInput); i++) {
+      output += (',') + i;
+    }
+
+  return output;
+};
