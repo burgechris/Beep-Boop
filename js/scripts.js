3eec8a687bfcc44d8041771aab213913761f8550
Chris Burge <topher.burge@gmail.com>
2019-08-23 10:32:39 -0700

    add response so that it shows up on the page.

--- js/scripts.js
+++ js/scripts.js
@@ -4,24 +4,22 @@ $(document).ready(function() {
   $('#numberChecker').submit(function(event) {
     event.preventDefault();
     var userNumber = $('input#random').val();
-    var range = '';
+
+    // var range = function(start_num, end_num) {
+    //
+    // }
 
     for(var i = 0; i <= userNumber; i += 1) {
       if (userNumber.includes('3')){
-        var dave = "I'm sorry, Dave. I'm afraid I can't do that."
-        console.log(dave);
+        var output = "I'm sorry, Dave. I'm afraid I can't do that."
       } else if (userNumber.includes('2')) {
-        var beep = 'Boop!';
-        console.log(beep);
+        var output = 'Boop!';
       } else if (userNumber.includes('1')) {
-        var beep = 'Beep!';
-        console.log(beep);
+        var output = 'Beep!';
       }
     }
+    $('#output').text(output);
   });
 });
 
 //Business Logic
-
-function outputNumber(userNumber) {
-}
