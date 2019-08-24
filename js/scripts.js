c2d387a5660b18a0d48d1fc20372097db1296d11
Chris Burge <topher.burge@gmail.com>
2019-08-24 14:59:48 -0700

    reorder the function and add a second for loop.

--- js/scripts.js
+++ js/scripts.js
@@ -3,27 +3,31 @@
 $(document).ready(function() {
   $('#numberChecker').submit(function(event) {
     event.preventDefault();
-    var userInput = $('#random').val();
-    var display = result(userInput);
-    $('#output').text(display);
+    var userInput = parseInt($('#random').val());
+    var showme = boopiedoopie(userInput);
+
+    $('#output').text(showme);
 
   });
 });
 
 //Business Logic
 
-function result(userInput) {
-  if (userInput.includes('3')) {
-    return "I'm sorry, Dave. I'm afraid I can't do that."
-  } else if (userInput.includes('2')) {
-    return 'Boop!';
-  } else if (userInput.includes('1')) {
-    return 'Beep!';
-  }
-  var numberVersion = parseInt(userInput);
+function boopiedoopie(userInput) {
   var numbers = '';
-  for (var i = 0; i <= numberVersion; i++) {
+  for (var i = 0; i <= userInput; i++) {
     numbers += (i + ', ');
+  }
+
+  for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i].includes('3')) {
+      numbers.replace([i], "I'm sorry, Dave. I'm afraid I can't do that.");
+      console.log(numbers);
+    } else if (numbers[i].includes(2)) {
+      numbers[i] = 'Boop!';
+    } else if (numbers[i].includes(1)) {
+      numbers[i] = 'Beep!';
+    }
   }
 
   return numbers;
